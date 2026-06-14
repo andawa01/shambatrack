ff2c2ccf9bfb6111939a87b51019d135d2e6ad2a
andawa01 <evansandawa02@gmail.com>
2026-06-14 23:27:05 +0300

    update apis

--- shambatrack-backend/index.js
+++ shambatrack-backend/index.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import app from "./app.js";
 import connectMongoDB from "./config/mongodb.js";
+import { seedSystemAdmin } from "./seed/systemAdmin.js";
 
 dotenv.config();
 
@@ -8,6 +9,8 @@ const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   await connectMongoDB();
+
+  await seedSystemAdmin();
 
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
